f0e5585417c5805ada5d149c6264676649064511
tr1vector <tr1vector@outlook.com>
2018-03-02 21:19:21 -0700

    Kele User Story - Messaging - Create Messages Complete

--- lib/messaging.rb
+++ lib/messaging.rb
@@ -8,8 +8,15 @@ module Messaging
 		JSON.parse(response.body)
 	end
 
-	def create_message()
-
+	def create_message(sender_email, recipient_id, subject, body)
+		response = self.class.post(base_api_url("messages"), 
+		body: { 
+			"sender": sender_email,
+			"recipient_id": recipient_id,
+			"subject": subject,
+			"stripped-text": body 
+		}, 
+		headers: { "authorization" => @auth_token })
 	end
 end
 
