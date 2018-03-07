a2267a7a3c7b496458575495f5a28d04605209ac
tr1vector <tr1vector@outlook.com>
2018-03-07 08:32:50 -0700

    Kele User Story - Checkpoint Submission Complete

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -8,4 +8,16 @@ module Roadmap
 		response = self.class.get(base_api_url("checkpoints/#{id}"), headers: { "authorization" => @auth_token })
 		JSON.parse(response.body)
 	end
+
+	def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)
+		response = self.class.post(base_api_url("checkpoint_submissions"), 
+		body: { 
+			"checkpoint_id": checkpoint_id,
+			"assignment_branch": assignment_branch,
+			"assignment_commit_link": assignment_commit_link,
+			"comment": comment,
+			"enrollment_id": enrollment_id
+		}, 
+		headers: { "authorization" => @auth_token })
+	end
 end
